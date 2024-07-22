ba8658792414937c1d13da471a94fb58833f9696
daniel glassen <danielglassen489@gmail.com>
2024-07-23 03:26:32 +0500

    Update workflow to match next js configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	reactStrictMode: true,
-	webpack: (config) => {
-		config.resolve.alias['@'] = path.resolve(__dirname);
-		return config;
-	}
+  reactStrictMode: true,
+  output: 'export',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
 };
