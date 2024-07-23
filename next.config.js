a24da411f7bb0b50d7c2a6d9f23baf6621486629
daniel glassen <danielglassen489@gmail.com>
2024-07-23 05:06:16 +0500

    Verify Deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-	reactStrictMode: true,
-	output: 'export',
-	images: {
-		unoptimized: true
-	},
-	distDir: 'dist',
-	webpack: (config) => {
-		config.resolve.alias['@'] = path.resolve(__dirname);
-		return config;
-	}
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'out',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
